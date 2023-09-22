8fc78dd08057163e22ce7c7429b4e36770dccdd9
popovay <90705081+popovay@users.noreply.github.com>
2023-09-22 17:39:42 +0400

    Update main.cpp
    добавил цикл

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include "TXLib.h"
 #include <iostream>
 
 int main()
     {
     txCreateWindow (800, 600);
-    txSetFillColour(TX_WHITE);
+
+    while (1)
+    {
+        txSetFillColour(TX_WHITE);
     txClear();
+
+    }
 
 
     return 0;
